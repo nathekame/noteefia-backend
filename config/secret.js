e9efcc4c09b5b9c5e8c5fab22bb04818fbf99b2b
Norbert Osi Athekame <n.athekame@gmail.com>
2022-09-01 22:24:30 +0100

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -3,16 +3,14 @@ module.exports = {
   baseUrl: process.env.BASE_URL || 'http://localhost:5000',
   hostEmail: process.env.HOST_EMAIL,
   // hostEmail: 'CVR PORTAL n.athekame@gmail.com',
-  dbUser: process.env.DB_USER || 'pmauser',
-  dbPassword: process.env.DB_PASSWORD || 'password',
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
   // nodePort: process.env.NODE_PORT || '7000',
   nodePort: '7000',
   jwtKey: process.env.JWT_KEY || 'inhikhinlni',
-  serviceKey: process.env.SERVICE_KEY || 'TEST',
-  sendgridKey: process.env.SENDGRID_API_KEY,
-  hookKey: process.env.HOOK_API_KEY || 'digitalTEST',
+  serviceKey: process.env.SERVICE_KEY,
 
-  cryptoKey: process.env.CRYPTO_KEY || 'pWJP6kqmtQWjRRIqLc7pcxs01lwHzsr6',
+  cryptoKey: process.env.CRYPTO_KEY,
 
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey:
